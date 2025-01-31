84a001d91ec26ad6a9845a721db2d702b781fd35
RAFEEF <RAFEEF@10.0.0.65>
2025-01-31 16:14:03 -0500

    implemented getInfo()

--- src/demo1/DemoTest8.java
+++ src/demo1/DemoTest8.java
@@ -3,7 +3,18 @@ package demo1;
 public class DemoTest8 {
 	 public static void main (String args []) {
 		 System.out.println("JavaIsFun");
+		 System.out.println("Alhakam");
+		 
+		 String name = "Alhakam";
+		 int age = 9 ;
+		 
+		 System.out.println(name + "Alhakam" + age  );
 	 }
-	 
+	 public void getInfo() {
+		String name = "Alhakam";
+		int age = 9;
+		 
+		System.out.println(name + age);
+	 }
 
 }
